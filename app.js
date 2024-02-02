1084f7320acda42aa1d9eaf6eaabe1d68e5695b3
J <133668286+alswla@users.noreply.github.com>
2024-02-02 17:10:58 +0900

    Feat: loader 파일들 생성

--- app.js
+++ app.js
@@ -1,31 +1,14 @@
-const cors = require("cors");
-const logger = require("morgan");
+require("dotenv").config();
 const express = require("express");
 
 const app = express();
 
-app.use(logger("dev"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    methods: "GET, POST",
-    credential: true,
-    preflightContinue: true,
-  }),
-);
+const connectServer = require("./loaders/express");
+const connectRouters = require("./loaders/routes");
+const connectErrorMiddleware = require("./loaders/error");
 
-app.use((req, res, next) => {
-  next();
-});
-
-app.use((err, req, res, next)  => {
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
-
-  res.status(err.status || 500);
-  res.render("error");
-});
+connectServer(app);
+connectRouters(app);
+connectErrorMiddleware(app);
 
 module.exports = app;
