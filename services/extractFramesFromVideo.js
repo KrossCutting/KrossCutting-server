1082fa44e5ccfc58ef4650aa02e957f30bb2e39d
zzinLee <sujin951017@gmail.com>
2024-02-04 16:31:00 +0900

    Comment: TO DO 주석 추가

--- services/extractFramesFromVideo.js
+++ services/extractFramesFromVideo.js
@@ -1,6 +1,7 @@
 const { spawn } = require("child_process");
 const path = require("path");
 
+// TO DO: ffmpeg에 전달하는 배열인수를 반환하는 함수(duration 옵션 추가 등)
 async function extractFramesFromVideo(inputPath, framePath, startSecond, fps) {
   return new Promise((resolve, reject) => {
     const ffmpegProcess = spawn("ffmpeg", [
