5559b95fb4543c350dc8b5d9a1eada7088709e1b
J <133668286+alswla@users.noreply.github.com>
2024-02-04 14:52:15 +0900

    Refactor: 오디오 파일 확장자 및 키값 동적으로 처리

--- services/getMediaResources.js
+++ services/getMediaResources.js
@@ -18,19 +18,26 @@ async function getMediaResources(title, url, s3Client, folder) {
       filter: "audioonly",
     });
 
-    const audioFile = ytdl.downloadFromInfo(videoInfo, {
-      format: audioFormatInfo,
-    });
     const videoFile = ytdl.downloadFromInfo(videoInfo, {
       format: highQualityVideo,
+    });
+    const audioFile = ytdl.downloadFromInfo(videoInfo, {
+      format: audioFormatInfo,
     });
 
     const videoContentType =
       highQualityVideo.container === "mp4" ? "video/mp4" : "video/webm";
+    const audioContentType =
+      audioFormatInfo.container === "mp3" ? "audio/mpeg" : "audio/webm";
+
     const videoKey =
       highQualityVideo.container === "mp4"
         ? `${folder}/videos/${title}-original.mp4`
         : `${folder}/videos/${title}-original.webm`;
+    const audioKey =
+      audioFormatInfo.container === "mp3"
+        ? `${folder}/audios/${title}-original.mp3`
+        : `${folder}/audios/${title}-original.webm`;
 
     const videoUpload = new Upload({
       client: s3Client,
@@ -41,14 +48,13 @@ async function getMediaResources(title, url, s3Client, folder) {
         ContentType: videoContentType,
       },
     });
-
     const audioUpload = new Upload({
       client: s3Client,
       params: {
         Bucket: process.env.AWS_BUCKET,
-        Key: `${folder}/audios/${title}-original.mp3`,
+        Key: audioKey,
         Body: audioFile,
-        ContentType: "audio/mpeg",
+        ContentType: audioContentType,
       },
     });
 
