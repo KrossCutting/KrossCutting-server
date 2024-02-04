d851fac3cabae2f210525c3ceb5a5f20aa43bbc1
zzinLee <sujin951017@gmail.com>
2024-02-04 15:24:59 +0900

    Refactor: extractFramesFromVideo 프로미스를 반환하도록 변경

--- services/extractFramesFromVideo.js
+++ services/extractFramesFromVideo.js
@@ -1,33 +1,42 @@
 const { spawn } = require("child_process");
 const path = require("path");
 
-function extractFramesFromVideo(inputPath, framePath, startSecond, fps) {
-  const ffmpegProcess = spawn("ffmpeg", [
-    "-i",
-    inputPath,
-    "-vf",
-    `fps=${fps}`,
-    "-ss",
-    startSecond,
-    "-start_number",
-    "1",
-    path.join(framePath, `frame_%d.jpg`),
-  ]);
+async function extractFramesFromVideo(inputPath, framePath, startSecond, fps) {
+  return new Promise((resolve, reject) => {
+    const ffmpegProcess = spawn("ffmpeg", [
+      "-i",
+      inputPath,
+      "-vf",
+      `fps=${fps}`,
+      "-ss",
+      startSecond,
+      "-start_number",
+      "1",
+      path.join(framePath, `frame_%d.jpg`),
+    ]);
 
-  ffmpegProcess.stdout.on("data", (data) => {
-    console.log(`stdout: ${data}`);
-  });
+    ffmpegProcess.stdout.on("data", (data) => {
+      console.log(`stdout: ${data}`);
+    });
 
-  ffmpegProcess.stderr.on("data", (data) => {
-    console.error(`stdout: ${data}`);
-  });
+    ffmpegProcess.stderr.on("data", (data) => {
+      console.error(`stderr: ${data}`);
+    });
 
-  ffmpegProcess.on("close", (code) => {
-    console.log(`child process exited with code ${code}`);
-  });
+    ffmpegProcess.on("close", (code) => {
+      if (code === 0) {
+        console.log("Frame extraction completed successfully");
+        resolve();
+      } else {
+        console.error(`Error: ffmpeg process exited with code ${code}`);
+        reject();
+      }
+    });
 
-  ffmpegProcess.on("error", (err) => {
-    console.error(`Error: ${err.message}`);
+    ffmpegProcess.on("error", (err) => {
+      console.error(`Error: ${err.message}`);
+      reject();
+    });
   });
 }
 
