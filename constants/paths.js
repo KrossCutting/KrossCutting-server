ebb6a0cda2b215da7b3966cbc58904da40330e95
zzinLee <sujin951017@gmail.com>
2024-02-06 17:00:34 +0900

    Docs: constants/path 경로명 추가 및 수정

--- constants/paths.js
+++ constants/paths.js
@@ -1,15 +1,32 @@
 const path = require("path");
 
+const TEMP_DIR = path.join(__dirname, "../temp");
+
 const TEMP_DIR_VIDEOS = {
-  MAIN: path.join(__dirname, "../temp/videos/main/"),
+  VIDEOS: path.join(__dirname, "../temp/videos"),
+  MAIN: path.join(__dirname, "../temp/videos/main"),
   SUB_ONE: path.join(__dirname, "../temp/videos/sub-one"),
   SUB_TWO: path.join(__dirname, "../temp/videos/sub-two"),
 };
 
 const TEMP_DIR_AUDIOS = {
-  MAIN: path.join(__dirname, "../temp/audios/main/"),
+  AUDIOS: path.join(__dirname, "../temp/audios"),
+  MAIN: path.join(__dirname, "../temp/audios/main"),
   SUB_ONE: path.join(__dirname, "../temp/audios/sub-one"),
   SUB_TWO: path.join(__dirname, "../temp/audios/sub-two"),
 };
 
-module.exports = { TEMP_DIR_VIDEOS, TEMP_DIR_AUDIOS };
+const TEMP_DIR_FRAMES = {
+  FRAMES: path.join(__dirname, "../temp/frames"),
+  DIFF: path.join(__dirname, "../temp/frames/difference"),
+  MAIN: path.join(__dirname, "../temp/frames/main-contents"),
+  SUB_ONE: path.join(__dirname, "../temp/frames/sub-one-contents"),
+  SUB_TWO: path.join(__dirname, "../temp/frames/sub-two-contents"),
+};
+
+module.exports = {
+  TEMP_DIR_VIDEOS,
+  TEMP_DIR_AUDIOS,
+  TEMP_DIR_FRAMES,
+  TEMP_DIR,
+};
