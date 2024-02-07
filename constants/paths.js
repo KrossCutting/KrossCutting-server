29af88f3d706687b5644db96e11ced96ede2fa79
zzinLee <sujin951017@gmail.com>
2024-02-07 19:56:51 +0900

    Style: 상수명 변경

--- constants/paths.js
+++ constants/paths.js
@@ -3,21 +3,21 @@ const path = require("path");
 const TEMP_DIR = path.join(__dirname, "../temp");
 
 const TEMP_DIR_VIDEOS = {
-  VIDEOS: path.join(__dirname, "../temp/videos"),
+  FORDER: path.join(__dirname, "../temp/videos"),
   MAIN: path.join(__dirname, "../temp/videos/main"),
   SUB_ONE: path.join(__dirname, "../temp/videos/sub-one"),
   SUB_TWO: path.join(__dirname, "../temp/videos/sub-two"),
 };
 
 const TEMP_DIR_AUDIOS = {
-  AUDIOS: path.join(__dirname, "../temp/audios"),
+  FORDER: path.join(__dirname, "../temp/audios"),
   MAIN: path.join(__dirname, "../temp/audios/main"),
   SUB_ONE: path.join(__dirname, "../temp/audios/sub-one"),
   SUB_TWO: path.join(__dirname, "../temp/audios/sub-two"),
 };
 
 const TEMP_DIR_FRAMES = {
-  FRAMES: path.join(__dirname, "../temp/frames"),
+  FORDER: path.join(__dirname, "../temp/frames"),
   DIFF: path.join(__dirname, "../temp/frames/difference"),
   MAIN: path.join(__dirname, "../temp/frames/main-contents"),
   SUB_ONE: path.join(__dirname, "../temp/frames/sub-one-contents"),
