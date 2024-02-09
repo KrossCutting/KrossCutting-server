3f5b4068e9a6e215ce4cee8627ab38f69435cc6d
zzinLee <sujin951017@gmail.com>
2024-02-09 12:16:07 +0900

    Feat: 얼굴좌표찾기 기능 추가, 리팩터링완료

--- services/getFaceCoordinate.js
+++ services/getFaceCoordinate.js
@@ -3,32 +3,63 @@ const blazeface = require("@tensorflow-models/blazeface");
 const tf = require("@tensorflow/tfjs-node");
 const fs = require("fs").promises;
 
-function getFaceCoordinate(imgPath) {
-  // eslint-disable-next-line no-async-promise-executor
-  return new Promise(async (resolve, reject) => {
-    try {
-      const imgBuffer = await fs.readFile(imgPath);
-      const img = await loadImage(imgBuffer);
-      const canvas = createCanvas(img.width, img.height);
-      const ctx = canvas.getContext("2d");
+async function getFaceCoordinate(faceImgPath) {
+  let pixels;
 
-      ctx.drawImage(img, 0, 0, img.width, img.height);
+  try {
+    // TO DO: 파일 있는지 확인
 
-      const model = await blazeface.load();
-      const input = tf.browser.fromPixels(canvas);
-      const predictions = await model.estimateFaces(input, false);
-      const faceCoordinates = [];
+    const faceImgBuffer = await fs.readFile(faceImgPath);
+    const loadedFaceImg = await loadImage(faceImgBuffer);
+    const canvas = createCanvas(loadedFaceImg.width, loadedFaceImg.height);
+    const ctx = canvas.getContext("2d");
 
-      predictions.forEach((prediction, index) => {
-        faceCoordinates.push(prediction);
+    ctx.drawImage(
+      loadedFaceImg,
+      0,
+      0,
+      loadedFaceImg.width,
+      loadedFaceImg.height,
+    );
+
+    const faceModel = await blazeface.load();
+
+    pixels = tf.browser.fromPixels(canvas);
+
+    const predictions = await faceModel.estimateFaces(pixels, false);
+
+    if (predictions.length > 1) {
+      predictions.sort((previous, current) => {
+        const {
+          topLeft: [prevLeftX, prevTopY],
+          bottomRight: [prevRightX, prevBottomY],
+        } = previous;
+        const {
+          topLeft: [curLeftX, curTopY],
+          bottomRight: [curRightX, curBottomY],
+        } = current;
+
+        const prevArea = (prevRightX - prevLeftX) * (prevBottomY - prevTopY);
+        const curArea = (curRightX - curLeftX) * (curBottomY - curTopY);
+
+        return curArea - prevArea;
       });
-
-      resolve(faceCoordinates[0]);
-    } catch (err) {
-      console.error(`Error: ${err}`);
-      reject();
     }
-  });
+
+    if (predictions.length === 0) {
+      return null;
+    }
+
+    tf.dispose(pixels);
+
+    return predictions[0];
+  } catch (err) {
+    tf.dispose(pixels);
+
+    console.error(`에러 발생 ${err}`);
+
+    return null;
+  }
 }
 
 module.exports = getFaceCoordinate;
