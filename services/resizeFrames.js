18acf800ce8d48ef3beba77d61a5bf26e5848a62
zzinLee <sujin951017@gmail.com>
2024-02-09 12:52:16 +0900

    Feat: 이미지 조정 기능 추가

--- services/resizeFrames.js
+++ services/resizeFrames.js
@@ -1,68 +1,195 @@
 const sharp = require("sharp");
+const fs = require("fs");
 
 const getFaceCoordinate = require("./getFaceCoordinate");
 
-async function calculateRatio(imgPath1, imgPath2) {
-  // eslint-disable-next-line no-async-promise-executor
-  return new Promise(async (resolve, reject) => {
-    try {
-      const faceData1 = await getFaceCoordinate(imgPath1);
-      const faceData2 = await getFaceCoordinate(imgPath2);
+function cutFrameArguments(mainCoord, subCoord, imgMetadata) {
+  const {
+    topLeft: [mainLeftX, mainTopY],
+    bottomRight: [mainRightX, mainBottomY],
+  } = mainCoord;
+  const {
+    topLeft: [subLeftX, subTopY],
+    bottomRight: [subRightX, subBottomY],
+  } = subCoord;
+  const mainFaceCoord = {
+    x: (mainLeftX + mainRightX) / 2,
+    y: (mainTopY + mainBottomY) / 2,
+  };
+  const subFaceCoord = {
+    x: (subLeftX + subRightX) / 2,
+    y: (subTopY + subBottomY) / 2,
+  };
 
-      if (!faceData1 || !faceData2) {
-        console.log("조회된 얼굴이 없습니다");
-        return;
-      }
+  if (mainFaceCoord.x > subFaceCoord.x) {
+    if (mainFaceCoord.y > subFaceCoord.y) {
+      const resizeWidth = Math.floor(
+        imgMetadata.width - (mainFaceCoord.x - subFaceCoord.x),
+      );
+      const resizeHeight = Math.floor(
+        imgMetadata.height - (mainFaceCoord.y - subFaceCoord.y),
+      );
 
-      const {
-        topLeft: [leftX1, topY1],
-        bottomRight: [rightX1, bottomY1],
-      } = faceData1;
-
-      const {
-        topLeft: [leftX2, topY2],
-        bottomRight: [rightX2, bottomY2],
-      } = faceData2;
-
-      const [width1, width2] = [rightX1 - leftX1, rightX2 - leftX2];
-      resolve(width1 / width2);
-    } catch (err) {
-      reject();
+      return {
+        left: 0,
+        top: 0,
+        width: resizeWidth,
+        height: resizeHeight,
+      };
     }
-  });
-}
+    const resizeWidth = Math.floor(
+      imgMetadata.width - (mainFaceCoord.x - subFaceCoord.x),
+    );
+    const resizeHeight = Math.floor(
+      imgMetadata.height - (subFaceCoord.y - mainFaceCoord.y),
+    );
 
-async function resizeFrame(img1, img2) {
-  const ratio = await calculateRatio(img1, img2);
-
-  if (!ratio) {
-    console.error("계산된 ratio가 없습니다");
-    return;
+    return {
+      left: 0,
+      top: Math.floor(subFaceCoord.y - mainFaceCoord.y),
+      width: resizeWidth,
+      height: resizeHeight,
+    };
   }
+  if (mainFaceCoord.y > subFaceCoord.y) {
+    const resizeWidth = Math.floor(
+      imgMetadata.width - (subFaceCoord.x - mainFaceCoord.x),
+    );
+    const resizeHeight = Math.floor(
+      imgMetadata.height - (mainFaceCoord.y - subFaceCoord.y),
+    );
 
-  const [bigger, smaller] = ratio > 1 ? [img1, img2] : [img2, img1];
-  const metadataImg = await sharp(smaller).metadata();
+    return {
+      left: Math.floor(subFaceCoord.x - mainFaceCoord.x),
+      top: 0,
+      width: resizeWidth,
+      height: resizeHeight,
+    };
+  }
+  const resizeWidth = Math.floor(
+    imgMetadata.width - (subFaceCoord.x - mainFaceCoord.x),
+  );
+  const resizeHeight = Math.floor(
+    imgMetadata.height - (subFaceCoord.y - mainFaceCoord.y),
+  );
 
-  sharp(smaller)
-    .resize({
-      width: Math.round(metadataImg.width * ratio),
-      height: Math.round(metadataImg.height * ratio),
-    })
-    .extract({
-      width: metadataImg.width,
-      height: metadataImg.height,
-      left: Math.floor(((ratio - 1) * metadataImg.width) / 2),
-      top: Math.floor(((ratio - 1) * metadataImg.height) / 2),
-    })
-    .toFile(smaller, (err, info) => {
-      if (err) {
-        console.error("이미지 생성 중 오류가 발생하였습니다.");
-        console.error(err);
-      } else {
-        console.log("성공적으로 이미지가 생성되었습니다");
-        console.log(info);
-      }
-    });
+  return {
+    left: Math.floor(subFaceCoord.x - mainFaceCoord.x),
+    top: Math.floor(subFaceCoord.y - mainFaceCoord.y),
+    width: resizeWidth,
+    height: resizeHeight,
+  };
 }
 
-module.exports = resizeFrame;
+function verifySamePositionFace(mainCoord, subCoord, imgMetadata) {
+  const {
+    topLeft: [mainLeftX, mainTopY],
+    bottomRight: [mainRightX, mainBottomY],
+  } = mainCoord;
+  const {
+    topLeft: [subLeftX, subTopY],
+    bottomRight: [subRightX, subBottomY],
+  } = subCoord;
+  const mainFaceCoord = {
+    x: (mainLeftX + mainRightX) / 2,
+    y: (mainTopY + mainBottomY) / 2,
+  };
+  const subFaceCoord = {
+    x: (subLeftX + subRightX) / 2,
+    y: (subTopY + subBottomY) / 2,
+  };
+
+  const widthDiff =
+    Math.abs(mainFaceCoord.x - subFaceCoord.x) > imgMetadata.width / 4;
+  const heightDiff =
+    Math.abs(mainFaceCoord.y - subFaceCoord.y) > imgMetadata.height / 4;
+
+  return !(widthDiff || heightDiff);
+}
+
+async function resizeFrames(mainImg, subImg) {
+  try {
+    const mainFaceCoord = await getFaceCoordinate(mainImg);
+    const subFaceCoord = await getFaceCoordinate(subImg);
+    const imgMetadata = await sharp(mainImg).metadata();
+
+    if (!mainFaceCoord || !subFaceCoord) {
+      console.log("조회된 얼굴이 없습니다.");
+
+      return null;
+    }
+
+    if (!verifySamePositionFace(mainFaceCoord, subFaceCoord, imgMetadata)) {
+      console.log("같은 얼굴이 아닙니다.  임의로 사이즈를 조정합니다.");
+
+      const {
+        topLeft: [mainLeftX, mainTopY],
+        bottomRight: [mainRightX, mainBottomY],
+      } = mainFaceCoord;
+      const {
+        topLeft: [subLeftX, subTopY],
+        bottomRight: [subRightX, subBottomY],
+      } = subFaceCoord;
+
+      const widthMain = mainRightX - mainLeftX;
+      const widthSub = subRightX - subLeftX;
+      const ratio = widthMain / widthSub;
+
+      const [bigger, smaller] =
+        ratio > 1 ? [mainImg, subImg] : [subImg, mainImg];
+      const resizeRatio = ratio > 1 ? ratio : 1 / ratio;
+
+      sharp(smaller)
+        .resize(
+          Math.floor(imgMetadata.width * resizeRatio),
+          Math.floor(imgMetadata.height * resizeRatio),
+        )
+        .extract({
+          top: Math.floor(((resizeRatio - 1) * imgMetadata.width) / 2),
+          left: Math.floor(((resizeRatio - 1) * imgMetadata.height) / 2),
+          width: imgMetadata.width,
+          height: imgMetadata.height,
+        })
+        .toBuffer((err, buffer) => {
+          if (err) {
+            console.error(`이미지 생성 중 오류 발생: ${err.message}`);
+          } else {
+            fs.writeFile(smaller, buffer, (writeErr) => {
+              if (writeErr) {
+                console.error(`이미지 저장 중 오류 발생 ${writeErr.message}`);
+              } else {
+                console.log("성공적으로 이미지 저장 완료 되었습니다.");
+              }
+            });
+          }
+        });
+
+      return null;
+    }
+
+    sharp(subImg)
+      .extract(cutFrameArguments(mainFaceCoord, subFaceCoord, imgMetadata))
+      .resize(imgMetadata.width, imgMetadata.height)
+      .toBuffer((err, buffer) => {
+        if (err) {
+          console.error(`이미지 생성 중 오류 발생: ${err.message}`);
+        } else {
+          fs.writeFile(subImg, buffer, (writeErr) => {
+            if (writeErr) {
+              console.error(`이미지 저장 중 오류 발생 ${writeErr.message}`);
+            } else {
+              console.log("성공적으로 이미지 저장 완료 되었습니다.");
+            }
+          });
+        }
+      });
+
+    return null;
+  } catch (faceDetectError) {
+    console.error(`얼굴 좌표 연산 중 에러 발생 ${faceDetectError}`);
+
+    return null;
+  }
+}
+
+module.exports = resizeFrames;
