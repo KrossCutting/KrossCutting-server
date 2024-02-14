56826e41c0bf0ae891f6edc514e3ce7d506f4228
zzinLee <sujin951017@gmail.com>
2024-02-15 02:21:27 +0900

    Refactor: stringifyImgPath 리팩터링

--- src/util/stringifyImgPath.js
+++ src/util/stringifyImgPath.js
@@ -1,26 +1,20 @@
+/* eslint-disable */
 const { TEMP_DIR_FRAMES } = require("../constants/paths");
 
 function stringifyImgPath(videoLabel, frameNumber) {
-  let imgPathResult;
-
   switch (videoLabel) {
     case "main-contents":
-      imgPathResult = `${TEMP_DIR_FRAMES.MAIN}/frame_30fps_${frameNumber}.jpg`;
-      break;
+      return `${TEMP_DIR_FRAMES.MAIN}/frame_30fps_${frameNumber}.jpg`;
 
     case "sub-one-contents":
-      imgPathResult = `${TEMP_DIR_FRAMES.SUB_ONE}/frame_30fps_${frameNumber}.jpg`;
-      break;
+      return `${TEMP_DIR_FRAMES.SUB_ONE}/frame_30fps_${frameNumber}.jpg`;
 
     case "sub-two-contents":
-      imgPathResult = `${TEMP_DIR_FRAMES.SUB_TWO}/frame_30fps_${frameNumber}.jpg`;
-      break;
+      return `${TEMP_DIR_FRAMES.SUB_TWO}/frame_30fps_${frameNumber}.jpg`;
 
     default:
-      break;
+      return;
   }
-
-  return imgPathResult;
 }
 
 module.exports = stringifyImgPath;
