2273f64a6ef399b8a7f4dc6fcfc065a559a9e46b
sht02048 <sht02048@gmail.com>
2024-02-14 14:53:48 +0900

    Feat: 오디오 편집 미들웨어 생성

---
+++ middleware/adjustAudio.js
@@ -0,0 +1,87 @@
+/* eslint-disable */
+const path = require("path");
+const ensureDir = require("../util/ensureDir");
+const removeDir = require("../util/removeDir");
+const cutAudio = require("../services/cutAudio");
+const extractStartTime = require("../services/extractStartTime");
+const { TEMP_DIR_WAV, TEMP_DIR_WAV_CUT, TEMP_DIR_WAV_ADJUSTED } = require("../constants/paths");
+
+async function adjustAudio(req, res, next) {
+  try {
+    const videoLabels = Object.keys(req.body);
+    const audioPaths = {};
+
+    for (let i = 0; i < videoLabels.length; i += 1) {
+      const videoLabel = videoLabels[i];
+      const startPoint = req.body[videoLabel];
+
+      let inputPath = "";
+      let outputPath = "";
+      let adjustedOutputPath = ""
+
+      if (startPoint === "") {
+        continue;
+      }
+
+      switch(videoLabel) {
+        case "mainStartPoint":
+          adjustedOutputPath = path.join(TEMP_DIR_WAV_ADJUSTED.MAIN, "main-audio.wav");
+          inputPath = path.join(TEMP_DIR_WAV.MAIN, "main-audio.wav");
+          outputPath = path.join(TEMP_DIR_WAV_CUT.MAIN, "main-audio.wav");
+          audioPaths.mainAudio = { outputPath, adjustedOutputPath};
+          ensureDir(TEMP_DIR_WAV_CUT.MAIN);
+          ensureDir(TEMP_DIR_WAV_ADJUSTED.MAIN);
+          break;
+
+        case "subOneStartPoint":
+          adjustedOutputPath = path.join(TEMP_DIR_WAV_ADJUSTED.SUB_ONE, "sub-one-audio.wav")
+          inputPath = path.join(TEMP_DIR_WAV.SUB_ONE, "sub-one-audio.wav");
+          outputPath = path.join(TEMP_DIR_WAV_CUT.SUB_ONE, "sub-one-audio.wav");
+          audioPaths.subOneAudio = { outputPath, adjustedOutputPath };
+          ensureDir(TEMP_DIR_WAV_CUT.SUB_ONE);
+          ensureDir(TEMP_DIR_WAV_ADJUSTED.SUB_ONE);
+          break;
+
+        case "subTwoStartPoint":
+          adjustedOutputPath = path.join(TEMP_DIR_WAV_ADJUSTED.SUB_TWO, "sub-two-audio.wav")
+          inputPath = path.join(TEMP_DIR_WAV.SUB_TWO, "sub-two-audio.wav");
+          outputPath = path.join(TEMP_DIR_WAV_CUT.SUB_TWO, "sub-two-audio.wav");
+          audioPaths.subTwoAudio = { outputPath, adjustedOutputPath };
+          ensureDir(TEMP_DIR_WAV_CUT.SUB_TWO);
+          ensureDir(TEMP_DIR_WAV_ADJUSTED.SUB_TWO);
+          break;
+
+        default:
+          throw new Error(`Unknown VideoLabel: ${videoLabel}`);
+      }
+
+      await cutAudio(inputPath, outputPath, startPoint);
+    }
+
+    const audioList = Object.values(audioPaths).map((paths) => paths.outputPath);
+    const adjustedStartTimes = await extractStartTime(audioList);
+    const audioLabels = Object.keys(audioPaths);
+
+    for (let i = 0; i < adjustedStartTimes.length; i += 1) {
+      const adjustedStartTime = adjustedStartTimes[i]
+      const currentAudioLabel = audioLabels[i];
+      const { outputPath, adjustedOutputPath } = audioPaths[currentAudioLabel];
+
+      await cutAudio(outputPath, adjustedOutputPath, adjustedStartTime);
+    }
+
+    // To Do. 실제 작업시 주석해제 필요
+    // removeDir(TEMP_DIR_WAV.FOLDER);
+    // removeDir(TEMP_DIR_WAV_CUT.FOLDER);
+
+    res.locals.adjustedStartTimes = adjustedStartTimes;
+
+    next();
+  } catch (err) {
+    console.error(err);
+
+    res.status(500).send("fail");
+  }
+}
+
+module.exports = adjustAudio;
--- routes/compilations.js
+++ routes/compilations.js
@@ -2,7 +2,8 @@ const express = require("express");
 
 const router = express.Router();
 
-// To Do. 교차 편집 컨트롤러 삽입 필요
-// router.post("/", );
+const adjustAudio = require("../middleware/adjustAudio");
+
+router.post("/", adjustAudio);
 
 module.exports = router;
