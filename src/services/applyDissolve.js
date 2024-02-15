18d672f88cbff1010c6c00925a99d4ba2c4d8e49
zzinLee <sujin951017@gmail.com>
2024-02-16 03:14:55 +0900

    Refactor: 디졸브 기능 리팩터링 및 추가, 프레임수 지정 가능

--- src/services/applyDissolve.js
+++ src/services/applyDissolve.js
@@ -1,7 +1,9 @@
 const fs = require("fs").promises;
 const sharp = require("sharp");
 
-async function applyDissolve(previousFramePath, currentFramePath) {
+const stringifyImgPath = require("../util/stringifyImgPath");
+
+async function applyDissolveOneFrame(previousFramePath, currentFramePath) {
   const dissolvedBuffer = await sharp(previousFramePath)
     .modulate({ brightness: 0.9 })
     .composite([
@@ -14,6 +16,28 @@ async function applyDissolve(previousFramePath, currentFramePath) {
     .toBuffer();
 
   await fs.writeFile(currentFramePath, dissolvedBuffer);
+}
+
+function applyDissolve(frameNumber, subVideoLabel, DISSOLVE_FRAME) {
+  const dissolveList = new Array(DISSOLVE_FRAME).fill(0).map((_, index) => {
+    const mainFramePath = stringifyImgPath(
+      "main-contents",
+      frameNumber + index,
+    );
+    const subFramePath = stringifyImgPath(subVideoLabel, frameNumber + index);
+
+    return [mainFramePath, subFramePath];
+  });
+
+  const dissolvePromiseList = dissolveList.map(
+    async ([mainFramePath, subFramePath]) => {
+      await applyDissolveOneFrame(mainFramePath, subFramePath);
+
+      return subFramePath;
+    },
+  );
+
+  return dissolvePromiseList;
 }
 
 module.exports = applyDissolve;
