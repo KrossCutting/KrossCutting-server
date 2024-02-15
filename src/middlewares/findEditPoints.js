0e8e74e9663bd8c4f1e5336703e0e222f7c02099
J <133668286+alswla@users.noreply.github.com>
2024-02-16 03:19:06 +0900

    Style: 오탈자 수정

--- src/middlewares/findEditPoints.js
+++ src/middlewares/findEditPoints.js
@@ -71,8 +71,8 @@ async function findEditPoints(req, res, next) {
     singleShots[folderName] = singleShotFrameList;
   }
 
-  res.loacls.editPoints = editPoints;
-  res.loacls.singleShots = singleShots;
+  res.locals.editPoints = editPoints;
+  res.locals.singleShots = singleShots;
 
   next();
 }
