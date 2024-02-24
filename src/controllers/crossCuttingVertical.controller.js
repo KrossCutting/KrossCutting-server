18f6414bc4aa4ed67a92e15cf9307f273b066cb1
sht02048 <sht02048@gmail.com>
2024-02-25 01:42:09 +0900

    Feat: vertical 컨트롤러 추가

---
+++ src/controllers/crossCuttingVertical.controller.js
@@ -0,0 +1,76 @@
+const removeDir = require("../util/removeDir");
+const editFrames = require("../services/editFrames");
+const progressStatus = require("../routes/progressStatus");
+const stringifyImgPath = require("../util/stringifyImgPath");
+const exportFinalVideo = require("../services/exportFinalVideo");
+const getFinalVideoUrl = require("../services/getFinalVideoUrl");
+const assignEditPoints = require("../services/assignEditPoints");
+const { TEMP_DIR_FRAMES, TEMP_DIR } = require("../constants/paths");
+
+async function crossCuttingVertical(req, res, next) {
+  console.log("비디오 편집 시작");
+  progressStatus.stage = "editing";
+  const { selectedEditPoints } = req.body;
+  const { editPoints } = res.locals;
+
+  const { subOneFrames, subTwoFrames } = assignEditPoints(
+    editPoints,
+    selectedEditPoints,
+  );
+
+  if (subOneFrames && subTwoFrames) {
+    const subOneFrameList = Object.entries(subOneFrames);
+    const subTwoFrameList = Object.entries(subTwoFrames);
+
+    if (subOneFrameList.length) {
+      const subOnePromiseList = subOneFrameList.map(
+        async ([frameNumber, durationTime]) => {
+          const mainFramePath = stringifyImgPath("main-contents", frameNumber);
+          const subFramePath = stringifyImgPath(
+            "sub-one-contents",
+            frameNumber,
+          );
+
+          await editFrames(mainFramePath, subFramePath, durationTime);
+        },
+      );
+
+      await Promise.all(subOnePromiseList);
+    }
+
+    if (subTwoFrameList.length) {
+      const subTwoPromiseList = subTwoFrameList.map(
+        async ([frameNumber, durationTime]) => {
+          const mainFramePath = stringifyImgPath("main-contents", frameNumber);
+          const subFramePath = stringifyImgPath(
+            "sub-two-contents",
+            frameNumber,
+          );
+
+          await editFrames(mainFramePath, subFramePath, durationTime);
+        },
+      );
+
+      await Promise.all(subTwoPromiseList);
+    }
+  }
+
+  await exportFinalVideo(TEMP_DIR_FRAMES.MAIN);
+
+  progressStatus.stage = "exporting";
+
+  const s3ClientFinalVideoUrl = await getFinalVideoUrl();
+
+  console.log(s3ClientFinalVideoUrl);
+  console.log("비디오 추출 완료");
+
+  res.status(201).send({
+    lastResult: "success",
+    s3ClientFinalVideoUrl,
+  });
+
+  // 실제 작업시 주석해제 필요
+  // removeDir(TEMP_DIR);
+}
+
+module.exports = crossCuttingVertical;
