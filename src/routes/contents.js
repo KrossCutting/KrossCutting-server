48a8c147c3be3dd52ceb041704213bd6e35f8090
sht02048 <sht02048@gmail.com>
2024-08-13 13:51:53 +0900

    Refactor: 유튜브 다운로더 기능 제거

--- src/routes/contents.js
+++ src/routes/contents.js
@@ -3,7 +3,6 @@ const express = require("express");
 const router = express.Router();
 
 const uploadController = require("../controllers/upload.controller");
-const transferController = require("../controllers/transfer.controller");
 const uploadToLocal = require("../services/uploadToLocal");
 
 router.post(
@@ -15,7 +14,5 @@ router.post(
   ]),
   uploadController,
 );
-
-router.post("/urls", transferController);
 
 module.exports = router;
